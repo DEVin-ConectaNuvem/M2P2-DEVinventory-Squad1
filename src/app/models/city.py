6202e353d35bdf8fd9910e39846ae6787ef17cd3
mayconrcampos <maycon.campos@gmail.com>
2022-08-16 19:54:32 -0300

    C28 feat: relacionando models

--- src/app/models/city.py
+++ src/app/models/city.py
@@ -1,23 +1,23 @@
 from src.app import DB, MA
-#from src.app.models import State
+from src.app.models import State, state_share_schema
 
 class City(DB.Model):
     __tablename__ = 'cities'
     id = DB.Column(DB.Integer, autoincrement = True, primary_key = True)
-    #state_id = DB.Column(DB.Integer, DB.ForeignKey(State.id), nullable = False)
+    state_id = DB.Column(DB.Integer, DB.ForeignKey(State.id), nullable = False)
     name = DB.Column(DB.String(128), nullable = False)
 
-    #state = DB.relationship("State", foreign_keys=[state_id])
+    state = DB.relationship("State", foreign_keys=[state_id])
 
-    def __init__(self, id, name):  
-      self.id = id
+    def __init__(self, name, state):  
+      self.state = state
       self.name = name
 
 class CitySchema(MA.Schema):
-    #state = MA.Nested(state_share_schema)
+    state = MA.Nested(state_share_schema)
 
     class Meta: 
-        fields = ('id', 'description', "state")
+        fields = ('id', 'name', "state")
 
 city_share_schema = CitySchema()
 citys_share_schema = CitySchema(many = True)
--- src/app/models/state.py
+++ src/app/models/state.py
@@ -1,22 +1,22 @@
 from src.app import DB, MA
-#from src.app.models import Country
+from src.app.models import Country, country_share_schema
 
 class State(DB.Model):
     __tablename__ = 'states'
     id = DB.Column(DB.Integer, autoincrement = True, primary_key = True)
-    #country_id = DB.Column(DB.Integer, DB.ForeignKey(Country.id), nullable = False)
+    country_id = DB.Column(DB.Integer, DB.ForeignKey(Country.id), nullable = False)
     name = DB.Column(DB.String(128), nullable = False)
     initials = DB.Column(DB.String(2), nullable=False)
 
-    #country = DB.relationship("country", foreign_keys=[country_id])
+    country = DB.relationship("country", foreign_keys=[country_id])
 
-    def __init__(self, id, name, initials):  
-      self.id = id
+    def __init__(self, name, initials, country):  
       self.name = name
       self.initials = initials
+      self.country = country
 
 class StateSchema(MA.Schema):
-    #country = MA.Nested(country_share_schema)
+    country = MA.Nested(country_share_schema)
 
     class Meta: 
         fields = ('id', 'description', "state")
