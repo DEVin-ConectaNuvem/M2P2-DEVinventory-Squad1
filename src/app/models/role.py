f6d815099e7014494e5589a9c8fc7fe23669583c
mayconrcampos <maycon.campos@gmail.com>
2022-08-16 18:00:47 -0300

    C20 - criação da model role

---
+++ src/app/models/role.py
@@ -0,0 +1,18 @@
+from src.app import DB, MA
+
+
+class Role(DB.Model):
+    __tablename__ = 'roles'
+    id = DB.Column(DB.Integer, autoincrement = True, primary_key = True)
+    description = DB.Column(DB.String(128), nullable = False)
+
+    def __init__(self, id, description):  
+      self.id = id
+      self.description = description
+
+class RoleSchema(MA.Schema):
+    class Meta: 
+        fields = ('id', 'description')
+
+role_share_schema = RoleSchema()
+roles_share_schema = RoleSchema(many = True)
