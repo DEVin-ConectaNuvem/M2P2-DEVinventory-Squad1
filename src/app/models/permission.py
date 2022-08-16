3f707a1f7d3ab825a8d0b80053d4a635c2215195
mayconrcampos <maycon.campos@gmail.com>
2022-08-16 18:14:53 -0300

    C21 feat: criando model permission

---
+++ src/app/models/permission.py
@@ -0,0 +1,18 @@
+from src.app import DB, MA
+
+
+class Permission(DB.Model):
+    __tablename__ = 'permissions'
+    id = DB.Column(DB.Integer, autoincrement = True, primary_key = True)
+    description = DB.Column(DB.String(128), nullable = False)
+
+    def __init__(self, id, description):  
+      self.id = id
+      self.description = description
+
+class PermissionSchema(MA.Schema):
+    class Meta: 
+        fields = ('id', 'description')
+
+Permission_share_schema = PermissionSchema()
+Permissions_share_schema = PermissionSchema(many = True)
