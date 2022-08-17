9fe3bb09c481c6d181090a9b40f0208a679a1ea1
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-08-16 21:37:20 -0300

    C-03/C-24 features: Ler Descrição
    - Implementação do comando  poetry run flask populate_db;
    - Feito a função principal populate_db
    - Feita a implementação da seed populate_db_country

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -24,6 +24,7 @@ def create_app():
   CORS(app)
   app.config["Access-Control-Allow-Origin"] = "*"
   app.config["Access-Control-Allow-Headers"] = "Content-Type"
+  from src.app.models import country
 
 
   return app
