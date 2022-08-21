0763510d4c9dda17bf1e35a5c924befcb5773bf0
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-08-21 16:45:00 -0300

    c149 refact: adiciona schema model user

--- src/app/services/inventory_service.py
+++ src/app/services/inventory_service.py
@@ -3,8 +3,10 @@ from src.app.models.schemas.inventory_schema import inventory_create_schema
 
 
 def create_product(data):
+    try:
         inventory_create_schema.load(data)
         inventory = Inventory.seed(data)
         result = inventory_share_schema.dump(inventory)
         return result
-  
+    except Exception as e:
+        return {"error": f"{e}", "status_code": 500}
